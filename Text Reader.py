35dbb665206627a8f2f7d0d6e702d239fb259b28
Abdu <muhammadabduramen@gmail.com>
2026-01-20 19:31:43 +0000

    Updates text reader.py

--- Text Reader.py
+++ Text Reader.py
@@ -1,1 +1,18 @@
-print("")
+def get_input(filename):
+    file=open(f"{filename}")
+    file_list=file.readlines()
+    n=int(file_list[0].strip()) #removes "\n" from the first line and converts to int and stores under variable n
+    time, cost=file_list[1].split() 
+    time_max=int(time)
+    cost_max=int(cost)
+    activities=[] #creates list of activities that is blank
+    for line in file_list[2:]:
+        line.strip()
+        name, time, cost, enjoyment=line.split()
+        activities.append({"name": name, "time":time, "cost": cost, "enjoyment": enjoyment}) #adds dictionary to list of activities
+    print(activities)
+    file.close()
+    return n, time_max, cost_max, activities
+
+#print(get_input("Inputs/input_10.txt"))
+get_input("Inputs/input_10.txt")

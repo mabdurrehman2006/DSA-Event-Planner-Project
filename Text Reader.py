aa53733c22ec4e16dd8ef069d16147dd41e7b1e0
Abdu <muhammadabduramen@gmail.com>
2026-01-20 19:39:13 +0000

    modified:   Text Reader.py

--- Text Reader.py
+++ Text Reader.py
@@ -3,13 +3,13 @@ def get_input(filename):
     file=open(f"{filename}") #opens file
     file_list=file.readlines() #reads all lines in file
     n=int(file_list[0].strip()) #removes "\n" from the first line and converts to int and stores under variable n
-    time, cost=file_list[1].split() #splits 2nd line and assigns to variables time and cost
+    time, cost=file_list[1].split() #splits 2nd line and assigns elements to variables time and cost
     time_max=int(time) #converts time to int
     cost_max=int(cost) #converts cost to int
     activities=[] #creates list of activities that is blank
     for line in file_list[2:]:
         line.strip()
-        name, time, cost, enjoyment=line.split()
+        name, time, cost, enjoyment=line.split() #splits line and assigns elements to variables name, time, cost and enjoyment
         activities.append({"name": name, "time":time, "cost": cost, "enjoyment": enjoyment}) #adds dictionary to list of activities
     file.close() #closes file
     return n, time_max, cost_max, activities 
